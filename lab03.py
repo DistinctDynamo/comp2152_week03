d93b8898b714dd8ad6e9726c40413b8308fa2e0a
DistinctDynamo <nguyensteven578@gmail.com>
2025-01-24 09:29:00 -0500

    defined a get combat strength function

--- lab03.py
+++ lab03.py
@@ -7,3 +7,13 @@ weapons = ["fist","Knife","Club","Gun","Bomb","Nuclear Bomb"]
 # Display available weapons
 print("Available Weapons: ", ','.join(weapons))
 
+def get_combat_strength(prompt):
+    while True:
+        try:
+            value=int(input(prompt))
+            if 1 <= value <= 6:
+                return value
+            else:
+                print("invalid input! Please enter a number between 1-6.")
+        except ValueError:
+            print("Invalid input.")
